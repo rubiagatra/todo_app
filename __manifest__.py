e072e6705c2b58dba2d23cd44b933d42740a324f
Doni Rubiagatra <rubiagatra@gmail.com>
2017-01-31 08:13:30 +0700

    todo_app from odoo essential development
    Initial commit

---
+++ __manifest__.py
@@ -0,0 +1,9 @@
+{
+    'name': 'To-Do Application',
+    'description': 'Manage your personal Tasks with this module.',
+    'author': 'Doni Rubiagatra',
+    'depends': ['mail'],
+    'application': True,
+    'data':['views/todo_view.xml'],
+            # 'security/ir.model.access.csv'],
+}
